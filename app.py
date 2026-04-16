3fcafd66d23f816189ae64612ac80230f9b19fc1
Sotirios Alpanis <salpanis@slv.vic.gov.au>
2026-04-16 14:03:55 +1000

    Fleshed out transcribe to work with an array

--- app.py
+++ app.py
@@ -1,29 +1,50 @@
 import gradio as gr
 import torch
-from transformers import pipeline
+
+import numpy as np
 
 import random
 
-MODEL_NAME = "openai/whisper-large-v3-turbo"
-BATCH_SIZE = 8
+from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
+
+processor = AutoProcessor.from_pretrained("openai/whisper-tiny")
+model = AutoModelForSpeechSeq2Seq.from_pretrained("openai/whisper-tiny")
+
 
 device = 0 if torch.cuda.is_available() else "cpu"
 
-pipe = pipeline(
-    task="automatic-speech-recognition",
-    model=MODEL_NAME,
-    chunk_length_s=30,
-    device=device,
-)
 
+def convert_audio(audio_input):
 
-# @spaces.GPU
-def transcribe(inputs):
-    if inputs is None:
-        raise gr.Error("No audio file submitted! Please upload or record an audio file before submitting your request.")
+    sample_rate, sample_array = audio_input
+    if sample_array.ndim > 1:
+        sample_array = sample_array.mean(axis=1)
+        
+    sample_array = sample_array.astype(np.float32)
+    sample_array /= np.max(np.abs(sample_array))
 
-    text = pipe(inputs, batch_size=BATCH_SIZE, return_timestamps=True)["text"]
-    return  [text, 'placeholder']
+    return sample_rate,  sample_array
+
+def transcribe(audio_input):
+
+    try:
+
+        model.config.forced_decoder_ids = None
+
+        sample_rate, sample_array = convert_audio(audio_input)
+
+        input_features = processor(sample_array, sampling_rate=sample_rate, return_tensors="pt").input_features
+
+        predicted_ids = model.generate(input_features)
+    # transcription = processor.batch_decode(predicted_ids, skip_special_tokens=False)
+
+        transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
+
+    except Exception as e:
+        gr.Error(f"An error occurred: {e}")
+
+    return transcription
+
 
 def select_quote():
 
